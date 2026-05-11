d1d34378c5db9de806150a1130bf8a006c0f67be
Rofak <rofak.man@ascendcorp.com>
2026-05-11 17:03:43 +0700

    chore: chunking_strategy

--- app/services/open_ai_servcie.py
+++ app/services/open_ai_servcie.py
@@ -48,7 +48,12 @@ class OpenAIService:
                     model="gpt-4o-transcribe-diarize",
                     file=audio_file,
                     response_format="diarized_json",
-                    chunking_strategy="auto",
+                    chunking_strategy={
+                        "type": "server_vad",
+                        "threshold": 0.4,
+                        "prefix_padding_ms": 300,
+                        "silence_duration_ms": 400
+                    },
                 )
 
             # 4. Process result
